aed007c34c8395799fb9738963c5bbae751dd773
pickolukas <picko.email.cz>
2021-02-27 18:57:08 +0100

    summary

--- src/component/RegisterList.tsx
+++ src/component/RegisterList.tsx
@@ -20,12 +20,11 @@ const { Title } = Typography;
   }
 
 const RegisterList: React.FC<Props> = (Props) => {
-    //const [data, setData] = useState(purchases);
     const [rewriteText, setRewriteText] = useState('');
 
     function handleCLick(itemId: string, itemName: string) {
         let newArr = Props.data.map((item) => {
-            if (itemId == item.id) {
+            if (itemId === item.id) {
                 return { ...item, ['name']: itemName };
             } else {
                 return item;
@@ -47,8 +46,8 @@ const RegisterList: React.FC<Props> = (Props) => {
                     <List.Item.Meta
                       avatar={
                         <Avatar>
-                            {(item.type=='Nákup')&&<Icon type="dollar" theme="twoTone"/>}
-                            {(item.type=='Pronájem')&&<Icon type="clock-circle" theme="twoTone" />}
+                            {(item.type==='Nákup')&&<Icon type="dollar" theme="twoTone"/>}
+                            {(item.type==='Pronájem')&&<Icon type="clock-circle" theme="twoTone" />}
                         </Avatar>}
                       title={<Title
                         editable={{
@@ -58,7 +57,7 @@ const RegisterList: React.FC<Props> = (Props) => {
 
                       description={item.type}
                     />
-                    <div>{item.price} {(item.type=='Nákup') ? ' Kč':'Kč/měsíc'}</div>
+                    <div>{item.price} {(item.type==='Nákup') ? ' Kč':'Kč/měsíc'}</div>
                     <Button onClick={(event: React.MouseEvent<HTMLElement>) => 
                         {handleCLick('001', 'ani hovno')}}>
                             Změň
