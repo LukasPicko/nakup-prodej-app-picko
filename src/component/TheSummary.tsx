3dcabc26471a8b8e209b3e071ae9822cda6f50a6
Picko <picko@cotrex.cz>
2021-03-05 09:45:06 +0100

    summary final

--- src/component/TheSummary.tsx
+++ src/component/TheSummary.tsx
@@ -1,33 +1,73 @@
-import React from 'react';
-import {Card} from 'antd';
+import React, {useState, useEffect} from 'react';
+import {Card, Typography} from 'antd';
+import { DataProps } from './../types/types';
+const {Text} = Typography;
+
+const TheSummary: React.FC<DataProps> = (props) => {
+
+    const[purchaseMax, setPurchaseMax] = useState(0)
+    const[purchaseSum, setPurchaseSum] = useState(0)
+    const[purchaseAvg, setPurchaseAvg] = useState(0)
+    const[purchaseCount, setPurchaseCount] = useState(0)
+    const[leaseMax, setLesaeMax] = useState(0)
+    const[leaseSum, setLeaseSum] =useState(0)
+    const[leaseAvg, setLeaseAvg] = useState(0)
+    const[leaseCount, setLeaseCount] = useState(0)
+
+    useEffect(() => {
+      setPurchaseMax(maxFce('Nákup'));
+      setPurchaseSum(sumFce('Nákup'));
+      setPurchaseAvg(avgFce('Nákup'));
+      setPurchaseCount(countFce('Nákup'));
+      setLesaeMax(maxFce('Pronájem'));
+      setLeaseSum(sumFce('Pronájem'));
+      setLeaseAvg(avgFce('Pronájem'));
+      setLeaseCount(countFce('Pronájem'));
+
+    },[]);
+
+    const maxFce = (par:string) => {
+      return  Math.max(...props.data.filter(item => item.type===par).map(item => (item.price)))
+    }
+
+    const sumFce = (par:string) => {
+      return props.data.filter(item => item.type===par).map(item => item.price).reduce((prev, curr) => prev + curr)
+    }
+
+    const avgFce = (par:string) => {
+      return sumFce(par)/props.data.filter(item => item.type===par).length
+    }
+
+    const countFce = (par:string) => {
+      return props.data.filter(item => item.type===par).length
+    }
 
 
-interface Props {
-  data: {
-    id: string;
-    type: string;
-    name: string;
-    price: number;
-}[],
-setData: React.Dispatch<React.SetStateAction<{
-    id: string;
-    type: string;
-    name: string;
-    price: number;
-}[]>>
-}
-const TheSummary: React.FC<Props> = () => {
     return (
       <Card title="Sumář">
         <Card type="inner" title="Nákupy">
-          Inner Card content
+          <Text>Nákupy maximální cena {purchaseMax}</Text>
+          <br />
+          <Text>Nákupy průměrná cena {purchaseAvg}</Text>
+          <br />
+          <Text>Nákupy celková cena{purchaseSum}</Text>
+          <br />
+          <Text>Nákupy počet{purchaseCount}</Text>
         </Card>
         <Card
           style={{ marginTop: 16 }}
           type="inner"
           title="Pronájmy"
         >
-          Inner Card content
+          <Text>Pronájmy maximální cena {leaseMax}</Text>
+          <br />
+          <Text>Pronájmy průměrná cena {leaseAvg}</Text>
+          <br />
+          <Text>Pronájmy celková cena {leaseSum}</Text>
+          <br />
+          <Text>Pronájmy počet {leaseCount}</Text>
+          
+          
         </Card>
   </Card>
     );
