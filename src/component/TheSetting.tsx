64421cfaa697d418cca561665a077d23f6a2bf5b
Picko <picko@cotrex.cz>
2021-03-09 10:00:41 +0100

    change filter button color

--- src/component/TheSetting.tsx
+++ src/component/TheSetting.tsx
@@ -22,14 +22,14 @@ const TheSetting: React.FC<CommonProps> = (Props) => {
     return (
       <div>
           {changeButton && 
-                    <Button onClick={handleOnOffFilter} type="primary" shape="circle" icon="filter" size='default' />
-                }
-                {!changeButton &&     
                     <Button onClick={handleOnOffFilter} type="default" shape="circle" icon="filter" size='default' />
                 }
-            <Button shape="round" onClick={showModal}>+Přidat</Button>{''}
-            <Button shape="circle">Cz</Button>
-            <Button shape="circle">En</Button>
+                {!changeButton &&     
+                    <Button onClick={handleOnOffFilter} type="primary" shape="circle" icon="filter" size='default'  style={{ background: '#FF9F33', borderColor: '#FF9F33' }}/>
+                }
+            <Button shape="round" onClick={showModal} style={{ background: '#FF9F33', borderColor: '#FF9F33', color: '#000000'}}>+Přidat</Button>{''}
+            <Button shape="circle" style={{ background: '#FF9F33', borderColor: '#FF9F33', color: '#000000'}}>Cz</Button>
+            <Button shape="circle" style={{ background: '#FF9F33', borderColor: '#FF9F33', color: '#000000' }}>En</Button>
             <Modal
                 title="Přidat položku"
                 visible={visibleModalForm}
